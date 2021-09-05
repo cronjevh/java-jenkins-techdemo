813495d4f1c648423094dc68866860d51aed08cf
Cronje van Heerden <cronjeVH@CCHP.onmicrosoft.com>
2021-09-05 20:03:58 +0200

    should have used helm from the start

---
+++ build/jcasc/jenkins-jcasc-helm-install.ps1
@@ -0,0 +1,13 @@
+# From https://github.com/jenkinsci/helm-charts/tree/main/charts/jenkins
+# Requires kubernetes cluster with connected kubectl and helm3
+# helm repo add jenkins https://charts.jenkins.io
+# helm repo update
+
+kubectl create namespace jcasc
+
+helm upgrade --install jcasc  `
+    -n jcasc `
+    -f values.yaml `
+    jenkins/jenkins
+
+helm delete jcasc -n jcasc
